a33b77a4cac50adb04142a3cd33aa0abbe5c6e54
Matthew Davis <mdavis0351@gmail.com>
2018-10-03 14:11:11 +0530

    added code, edited filenames, changed the notes page

---
+++ insertmany.py
@@ -0,0 +1,31 @@
+# import the pymongo library
+from pymongo import MongoClient
+
+
+# create a new database
+newclient = MongoClient('localhost', 27017)
+newdb = newclient["pythondb"]
+# create new colleciton
+heroes = newdb["heroes"]
+
+# insert only one record
+goodguys = [
+{"fname":"clark", "lname":"kent", "alias":"superman", "interests":[
+"phone booths", "flying", "leaping"]}
+{"fname":"peter", "lname":"parker", "alias":"spiderman", "interests":[
+"pictures", "webs", "science"]}
+{"fname":"steve", "lname":"rogers", "alias":"captian america", "interests":[
+"freedom", "taxes", "service"]}
+{"fname":"tony", "lname":"stark", "alias":"iron man", "interests":[
+"metal", "money", "microcomputers"]}
+{"fname":"clint", "lname":"barton", "alias":"hawkeye", "interests":[
+"archery", "birds", "flying"]}
+{"fname":"wanda", "lname":"maximoff", "alias":"scarlet witch", "interests":[
+"fire", "halloween", "the force"]}
+{"fname":"natasha", "lname":"romanoff", "alias":"black widow", "interests":[
+"nighttime", "spiders", "webs"]}
+{"fname":"diana", "lname":, "alias":"wonder woman", "interests":[
+"wonderful", "gold", "justice"]}
+]
+
+heroes.insert_many(goodguys)
--- insertone.py
+++ insertone.py
@@ -1,2 +1,15 @@
 # import the pymongo library
 from pymongo import MongoClient
+
+
+# create a new database
+newclient = MongoClient('localhost', 27017)
+newdb = newclient["pythondb"]
+# create new colleciton
+heroes = newdb["heroes"]
+
+# insert only one record
+hulk = {"fname":"bruce", "lname":"banner", "alias":"hulk", "interests":[
+"smashing", "anger", "new clothes"]}
+
+heroes.insert_one(hulk)
