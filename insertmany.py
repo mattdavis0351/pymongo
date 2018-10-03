4e193b59430c3e3ce1c131be49912174bc83c011
mattdavis0351 <mdavis0351@gmail.com>
2018-10-03 15:19:11 +0000

    fixed typos

--- insertmany.py
+++ insertmany.py
@@ -11,22 +11,22 @@ heroes = newdb["heroes"]
 # insert only one record
 goodguys = [
 {"fname":"clark", "lname":"kent", "alias":"superman", "interests":[
-"phone booths", "flying", "leaping"]}
+"phone booths", "flying", "leaping"]},
 {"fname":"peter", "lname":"parker", "alias":"spiderman", "interests":[
-"pictures", "webs", "science"]}
+"pictures", "webs", "science"]},
 {"fname":"steve", "lname":"rogers", "alias":"captian america", "interests":[
-"freedom", "taxes", "service"]}
+"freedom", "taxes", "service"]},
 {"fname":"tony", "lname":"stark", "alias":"iron man", "interests":[
-"metal", "money", "microcomputers"]}
+"metal", "money", "microcomputers"]},
 {"fname":"clint", "lname":"barton", "alias":"hawkeye", "interests":[
-"archery", "birds", "flying"]}
+"archery", "birds", "flying"]},
 {"fname":"matt", "lname":"davis", "alias":"fat turtle", "interests":[
-"shells", "strawberries", "pizza"]}
+"shells", "strawberries", "pizza"]},
 {"fname":"wanda", "lname":"maximoff", "alias":"scarlet witch", "interests":[
-"fire", "halloween", "the force"]}
+"fire", "halloween", "the force"]},
 {"fname":"natasha", "lname":"romanoff", "alias":"black widow", "interests":[
-"nighttime", "spiders", "webs"]}
-{"fname":"diana", "lname":, "alias":"wonder woman", "interests":[
+"nighttime", "spiders", "webs"]},
+{"fname":"diana", "lname":"", "alias":"wonder woman", "interests":[
 "wonderful", "gold", "justice"]}
 ]
 
